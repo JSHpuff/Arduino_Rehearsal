01ea1b0415812ab2ff716f3e62114c7bc1b86aee
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-05-19 10:33:15 +0800

    update the temp code and the image

---
+++ 01_blink.ino
@@ -0,0 +1,25 @@
+int led_red = 		0;
+int led_yellow = 	1;
+int led_green =		2;
+
+void setup() {
+	// Setup all the LEDs as OUTPUT
+	pinMode(led_red, 	OUTPUT);
+	pinMode(led_yellow, OUTPUT);
+	pinMode(led_green,	OUTPUT);
+}
+
+void loop() {
+	// Turn the green LED on and the other LEDs off
+	digitalWrite(led_red, 		LOW);
+	digitalWrite(led_yellow, 	LOW);
+	digitalWrite(led_green, 	HIGH);
+	delay(2000);
+
+	// Turn the yellow LED on and the other LEDs off
+	digitalWrite(led_red,		LOW);
+	digitalWrite(led_yellow, 	HIGH);
+	digitalWrite(led_green, 	LOW);
+	delay(1000);
+	
+}
