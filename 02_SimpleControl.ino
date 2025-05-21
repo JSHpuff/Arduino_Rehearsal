58ba03191f6ebbb4fd6c748a980d48267bda0fe0
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-05-21 16:41:01 +0800

    update the 02 simple control

---
+++ 02_SimpleControl.ino
@@ -0,0 +1,30 @@
+/**
+ * CopyRight:
+ * This code is provide by TinkerCAD
+ */
+
+#include <Servo.h>
+
+servo myServo;
+
+int const potPin = A0; 	//analog pin A
+int potVal;				//stores value of pot
+int angle = 0;				//stores position of servo
+
+void setup() {
+  myServo.attach(9);	//tells board which pin the servo is on
+  Serial.begin(9600);	
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  potVal = analogRead(potPin);
+  Serial.print("potVal: ");
+  Serial.print(potVal);
+  
+  angle = map(potVal, 0, 1023, 0, 179);	//angle is the pot value re-scaled to 0-179 degrees
+  Serial.print("angle: ");
+  Serial.println(angle);
+  myServo.write(angle);
+  delay(15);
+}
--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 For training arduino code
 
 ## Description
-1. 01_blink - Using simple GPIO control the LED.  
+1. 01_Blink - Using simple GPIO control the LED.  
 ![Image_from_tinkercad_01](/img/01_blink.PNG)
+2. 02_SimpleControl - Using potentiometer **analog** value controling servo angle.  
+![Image_from_tinkercad_02](/img/01_SimpleControl.PNG)
 
 # NOTE
 - All the Arduino sample codes' copyright are own by Arduino.
